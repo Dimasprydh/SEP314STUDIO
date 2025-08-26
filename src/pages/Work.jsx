7e7b5e38b3add6c90568495ffc227c87f06e7b6f
Dimasprydh <160858523+Dimasprydh@users.noreply.github.com>
2025-08-26 19:31:41 +0700

    Update Work.jsx

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -6,7 +6,7 @@ const projects = [
     slug: "mediocre",
     title: "MEDIOCRE",
     years: "WEB / 2023—NOW",
-    img: "../assets/portofolio-website/mediocre.png",
+    img: "/assets/portofolio-website/mediocre.png",
     href: "https://mediocreq.com/",
   },
   {
