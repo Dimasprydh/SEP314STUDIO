b6723371e80fc6bfcc16eb12c4d376d6359714f2
Dimasprydh <160858523+Dimasprydh@users.noreply.github.com>
2025-08-26 19:20:11 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -46,7 +46,7 @@ export default function About() {
               >
                 {/* pakai mp4 utama; mov sebagai fallback kalau perlu */}
                 <source
-                  src="${BASE}/assets/about-image-video/profile.mp4"
+                  src="${BASE}assets/about-image-video/profile.mp4"
                   type="video/mp4"
                 />
                 <source
