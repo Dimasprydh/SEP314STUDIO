12264f08fa67a4a18d4659fa565a5504ee0a2ba5
Dimasprydh <160858523+Dimasprydh@users.noreply.github.com>
2026-05-22 21:57:40 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Theme from "./layout/theme.jsx";
 import Home from "./pages/home.jsx";
 import About from "./pages/About.jsx";
 import Work from "./pages/Work.jsx";
+import Service from "./pages/Service";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/work" element={<Work />} />
         <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
         <Route path="*" element={<Home />} />
       </Route>
     </Routes>
