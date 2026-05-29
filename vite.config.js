b889303add0c410d2e7e458108c6b3313ad2d67d
Dimasprydh <160858523+Dimasprydh@users.noreply.github.com>
2026-05-29 13:38:20 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GANTI sesuai nama repo GitHub-mu:
-const repo = "SEP314STUDIO";
-
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages butuh base path ini (untuk asset, import.meta.env.BASE_URL, dll)
-  base: `/${repo}/`,
+  base: "/",
 });
